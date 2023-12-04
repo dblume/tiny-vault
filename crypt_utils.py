79c0858580dc54736f756f57590379957bd8efb9
dblume <david.blume@gmail.com>
2023-12-03 20:09:21 -0800

    Mostly PEP-8

--- crypt_utils.py
+++ crypt_utils.py
@@ -20,9 +20,9 @@ import time
 import urllib
 import base64
 
-class crypt_util_error( Exception ): pass
+class crypt_util_error(Exception): pass
 
-def encrypt_file( key, in_file, out_filename, chunksize=64*1024 ):
+def encrypt_file(key, in_file, out_filename, chunksize=64*1024):
     """ Encrypts a file using AES (CBC mode) with the
         given key.
 
@@ -43,27 +43,28 @@ def encrypt_file( key, in_file, out_filename, chunksize=64*1024 ):
             sizes can be faster for some files and machines.
             chunksize must be divisible by 16.
     """
-    iv = ''.join( chr( random.randint( 0, 0xFF )) for i in range(16) )
-    encryptor = AES.new( key, AES.MODE_CBC, iv )
+    iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
 #    filesize = os.path.getsize( in_filename )
 
     # This works for files and cString() objects
-    in_file.seek( 0, os.SEEK_END )
+    in_file.seek(0, os.SEEK_END)
     filesize = in_file.tell()
-    in_file.seek( 0, os.SEEK_SET )
+    in_file.seek(0, os.SEEK_SET)
 
     with open(out_filename, 'wb') as outfile:
         outfile.write(struct.pack('<Q', filesize))
         outfile.write(iv)
         while True:
-            chunk = in_file.read( chunksize )
-            if len( chunk ) == 0:
+            chunk = in_file.read(chunksize)
+            if len(chunk) == 0:
                 break
-            elif len( chunk ) % 16 != 0:
+            elif len(chunk) % 16 != 0:
                 chunk += ' ' * (16 - len(chunk) % 16)
-            outfile.write( encryptor.encrypt(chunk) )
+            outfile.write(encryptor.encrypt(chunk))
 
-def decrypt_file( key, in_filename, out_file, chunksize=24*1024 ):
+
+def decrypt_file(key, in_filename, out_file, chunksize=24*1024):
     """ Decrypts a file using AES (CBC mode) with the
         given key. Parameters are similar to encrypt_file,
         with one difference: out_filename, if not supplied
@@ -71,30 +72,32 @@ def decrypt_file( key, in_filename, out_file, chunksize=24*1024 ):
         (i.e. if in_filename is 'aaa.zip.enc' then
         out_filename will be 'aaa.zip')
     """
-    if not os.path.exists( in_filename ):
+    if not os.path.exists(in_filename):
         return False
-    with open( in_filename, 'rb' ) as infile:
+    with open(in_filename, 'rb') as infile:
         origsize = struct.unpack('<Q', infile.read(struct.calcsize('Q')))[0]
         iv = infile.read(16)
         decryptor = AES.new(key, AES.MODE_CBC, iv)
 
         while True:
-            chunk = infile.read( chunksize )
+            chunk = infile.read(chunksize)
             if len(chunk) == 0:
                 break
-            out_file.write( decryptor.decrypt( chunk ) )
-        out_file.truncate( origsize )
+            out_file.write(decryptor.decrypt(chunk))
+        out_file.truncate(origsize)
     return True
 
-def read_csv( filename ):
+
+def read_csv(filename):
     rows = []
     with open(filename, 'rb') as f:
-        rows = convert_csv_rows( f )
+        rows = convert_csv_rows(f)
     return rows
 
-def convert_csv_rows( f ):
+
+def convert_csv_rows(f):
     rows = []
-    reader = csv.reader( f )
+    reader = csv.reader(f)
     # Read in:
     # Type, Description, Username, Password, URL, Custom, Updated, Notes
     #
@@ -105,55 +108,58 @@ def convert_csv_rows( f ):
         row_num += 1
         if row_num == 1:
             continue
-        if len( row ) != 8:
-            raise crypt_util_error( "Row %d had %d fields instead of 8." % ( row_num, len( row ) ) )
-        stamp = time.mktime( time.strptime( row[6], '%d-%b-%y' ) )
-        row.insert( 0, row_num - 1 )
+        if len(row) != 8:
+            raise crypt_util_error("Row %d had %d fields instead of 8." % (row_num, len(row)))
+        stamp = time.mktime(time.strptime(row[6], '%d-%b-%y'))
+        row.insert(0, row_num - 1)
         row[7] = stamp
-        rows.append( row )
+        rows.append(row)
     return rows
 
-def encrypt_rows( enc_key, rows, out_filename ):
+
+def encrypt_rows(enc_key, rows, out_filename):
     f = cStringIO.StringIO()
-    writer = csv.writer( f )
-    writer.writerows( rows )
-    hash = hashlib.md5( f.getvalue() ).hexdigest()
-    with open( out_filename + '.hash', 'wb' ) as h:
-        h.write( hash )
-    encrypt_file( enc_key, f, out_filename + '.enc', 16 * 1024 )
+    writer = csv.writer(f)
+    writer.writerows(rows)
+    hash = hashlib.md5(f.getvalue()).hexdigest()
+    with open(out_filename + '.hash', 'wb') as h:
+        h.write(hash)
+    encrypt_fil(enc_key, f, out_filename + '.enc', 16 * 1024 )
     f.close()
 
-def decrypt_rows( enc_key, in_filename ):
+
+def decrypt_rows(enc_key, in_filename):
     """ returns True, rows upon success """
     rows = []
     f = cStringIO.StringIO()
-    if not decrypt_file( enc_key, in_filename + '.enc', f, 16 * 1024 ):
+    if not decrypt_file(enc_key, in_filename + '.enc', f, 16 * 1024):
         return False, rows
-    f.seek( 0 )
-    with open( in_filename + '.hash', 'rb' ) as h:
+    f.seek(0)
+    with open(in_filename + '.hash', 'rb') as h:
         file_hash = h.read()
-    if file_hash != hashlib.md5( f.getvalue() ).hexdigest():
+    if file_hash != hashlib.md5(f.getvalue()).hexdigest():
         return False, rows
-    reader = csv.reader( f )
+    reader = csv.reader(f)
     for row in reader:
-        rows.append( row )
+        rows.append(row)
     f.close()
     return True, rows
 
+
 if __name__=='__main__':
     import filelock
-    localdir = os.path.abspath( os.path.dirname( sys.argv[0] ) )
+    localdir = os.path.abspath(os.path.dirname(sys.argv[0]))
     username = 'test'
     print "Start!"
-    
-    rows = read_csv( os.path.join( localdir, 'splash_id_archive_CC_ID_fewer_columns.csv' ) )
 
-    bcrypt_salt = '$2a$12$0S7xZwmn6w4xmuY1x5X26O' # Mayde by bcrypt.gensalt()
-    enc_key = bcrypt.hashpw( "passw0rd", bcrypt_salt )[-32:]
-    encrypt_rows( enc_key, rows, os.path.join( localdir, 'data', username ) )
-    
-    with filelock.FileLock( os.path.join( localdir, 'data', username ) ) as lock:
-        succeeded, out_rows = decrypt_rows( enc_key, os.path.join( localdir, 'data', username ) )
+    rows = read_csv(os.path.join(localdir, 'splash_id_archive_CC_ID_fewer_columns.csv'))
+
+    bcrypt_salt = '$2a$12$0S7xZwmn6w4xmuY1x5X26O' # Made by bcrypt.gensalt()
+    enc_key = bcrypt.hashpw("passw0rd", bcrypt_salt)[-32:]
+    encrypt_rows(enc_key, rows, os.path.join(localdir, 'data', username))
+
+    with filelock.FileLock(os.path.join(localdir, 'data', username)) as lock:
+        succeeded, out_rows = decrypt_rows(enc_key, os.path.join(localdir, 'data', username))
     print "decrypt_rows succeeded =", succeeded
 
 #    my_hash = hashlib.sha224("Nobody inspects the spammish repetition").hexdigest()
@@ -163,9 +169,9 @@ if __name__=='__main__':
 #    hashed = bcrypt.hashpw( "password", bcrypt_salt )[-32:]
 
 
-#    session = base64.b64decode( urllib.unquote( cookie_val ) )
+#    session = base64.b64decode(urllib.unquote(cookie_val))
 #    urllib.quote_plus(string[, safe]) ?
-    
+
 #    print "bcrypt hashed", hashed
 
 #
@@ -178,9 +184,9 @@ if __name__=='__main__':
 #    print hashed
 
 
-#    encrypt_file( key, os.path.join( localdir, "source_file_for_encryption.txt" ) )
-#    decrypt_file( key, \
-#                  os.path.join( localdir, "source_file_for_encryption.txt.enc" ), \
-#                  os.path.join( localdir, "decrypted_file.txt" ) )
+#    encrypt_file(key, os.path.join( localdir, "source_file_for_encryption.txt"))
+#    decrypt_file(key, \
+#                  os.path.join(localdir, "source_file_for_encryption.txt.enc"), \
+#                  os.path.join(localdir, "decrypted_file.txt"))
 
     print "Done."
