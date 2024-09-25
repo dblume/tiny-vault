ffa0a5885242cbd4d2b615c6422684040e9dd3a6
dblume <david.blume@gmail.com>
2024-09-25 04:46:00 +0000

    Add another password generator
    This one creates 12-char passwords with upper, lower, punctuation and
    numbers.

--- gen_password.py
+++ gen_password.py
@@ -2,11 +2,11 @@
 #
 # http://www.evanfosmark.com/2009/07/creating-fake-words/
 
-import random
 import string
+import secrets
 from collections import defaultdict
 
-vowels = ['a', 'e', 'i', 'o', 'u' ]
+vowels = 'aeiou'
 
 digraphs = ['br', 'ch', 'ck', 'gh', 'ph', 'qu', 'sh', 'st',
             'th', 'wh', 'zh']
@@ -15,11 +15,11 @@ consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p',
               'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
 
 def _vowel():
-    return random.choice(vowels)
+    return secrets.choice(vowels)
 
 
 def _consonant():
-    return random.choice(consonants + digraphs)
+    return secrets.choice(consonants + digraphs)
 
 
 def _cv():
@@ -33,7 +33,7 @@ def _cvc():
 _syllables = [_vowel, _cv, _cvc]
 
 def _syllable():
-    return random.choice(_syllables)()
+    return secrets.choice(_syllables)()
 
 
 def _syllable_of_type(index, last_chars):
@@ -47,19 +47,32 @@ def _syllable_of_type(index, last_chars):
 
 
 def password():
-    """ This function generates a fake word by creating a few
-        random syllables and then joining them together.
+    """ Returns a somewhat random password, depending on the algorithm you choose.
     """
-    min_length = 5  # random.randint(7, 9)
+
+    # More secure, but less readable
+    alphabet = string.ascii_letters + string.digits + \
+               string.punctuation.translate(str.maketrans('', '', '\'"\\`'))
+    while False:
+        password = ''.join(secrets.choice(alphabet) for i in range(12))
+        if (any(c.islower() for c in password)
+               and any(c.isupper() for c in password)
+               and any(c in string.punctuation for c in password)
+               and any(c.isdigit() for c in password)):
+            return password
+
+    # More readable and "word selectable"
+    # Create some random syllables, and join them together into somthing wordish.
+    min_length = 5 + secrets.randbelow(3)
 
     choices = []
     prev_choice = 2
     prev_prev_choice = 2
     for i in range(min_length):
-        if prev_prev_choice < 2 and prev_choice == 0:
-            choice = random.randrange(1, 3)
+        if prev_prev_choice < 2 and prev_choice == 0:  # if ending with two vowels
+            choice = 1 + secrets.randbelow(2)  # exclude starting with a vowel
         else:
-            choice = random.randrange(3)
+            choice = secrets.randbelow(3)
         choices.append(choice)
         prev_prev_choice = prev_choice
         prev_choice = choice
@@ -69,21 +82,21 @@ def password():
     while len(word) < min_length:
         syllables.append(_syllable_of_type(choices.pop(0), word[:-2]))
         if syllables[-1].endswith('qu'):
-            syllables[-1] += random.choice('aeio')
+            syllables[-1] += secrets.choice('aeio')
         word += syllables[-1]
     # Capitalize something and stick a number and a '_' in there.
     index = 0  # random.randrange(len(syllables))
     syllables.insert(index, str.capitalize(syllables.pop(index)))
 
-    # Make passwords easier to remember, always #_... or ..._#
-    if random.randrange(2):
-        insertion_point = 0  # random.randrange(len(syllables) + 1)
+    # Add number and underscore to beginning or end, either  #_... or ..._#
+    if secrets.randbelow(2):
+        insertion_point = 0
         syllables.insert(insertion_point, '_')
     else:
-        insertion_point = len(syllables)  # random.randrange(len(syllables) + 1)
+        insertion_point = len(syllables)
         syllables.insert(insertion_point, '_')
-        insertion_point = len(syllables)  # random.randrange(len(syllables) + 1)
-    syllables.insert(insertion_point, random.choice(string.digits[2:]))
+        insertion_point = len(syllables)
+    syllables.insert(insertion_point, secrets.choice(string.digits[2:]))
     return "".join(syllables).replace('O', 'A').replace('I', 'Y')
 
 
